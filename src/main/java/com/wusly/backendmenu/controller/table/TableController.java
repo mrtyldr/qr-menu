2fdd698e6dd80c7130746e60982c43b21fa851f7
Murat YILDIRIM <murat.yildirim@parga.li>
2023-04-16 16:34:29 +0300

    add @CrossOrigin to TableController

--- src/main/java/com/wusly/backendmenu/controller/table/TableController.java
+++ src/main/java/com/wusly/backendmenu/controller/table/TableController.java
@@ -4,16 +4,14 @@ import com.wusly.backendmenu.domain.table.CreateTableCommand;
 import com.wusly.backendmenu.service.TableService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/table")
+@CrossOrigin
 public class TableController {
     private final TableService tableService;
 
