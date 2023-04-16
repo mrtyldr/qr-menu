18453d1c8373fe479ea99c1f0a80b2249a6888a1
Murat YILDIRIM <murat.yildirim@parga.li>
2023-04-16 20:12:09 +0300

    add @RequestMapping("/api/v1/qr")

--- src/main/java/com/wusly/backendmenu/controller/QrController.java
+++ src/main/java/com/wusly/backendmenu/controller/QrController.java
@@ -4,17 +4,15 @@ import com.wusly.backendmenu.service.QRService;
 import com.wusly.backendmenu.service.RestaurantService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.awt.image.BufferedImage;
 import java.security.Principal;
 
-@RestController("/api/v1/qr")
+@RestController
 @CrossOrigin
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/qr")
 public class QrController {
 
     private final QRService qrService;
