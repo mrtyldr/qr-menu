164b87f0756c1326d75e28e6e06c84f53165f9fd
Murat YILDIRIM <murat.yildirim@parga.li>
2023-04-17 16:27:15 +0300

    ORDER NOTIFICATIONS

--- src/main/java/com/wusly/backendmenu/repository/NotificationRepository.java
+++ src/main/java/com/wusly/backendmenu/repository/NotificationRepository.java
@@ -11,7 +11,7 @@ public interface NotificationRepository extends JpaRepository<Notification, UUID
     @Query("""
             select n from Notification n
             where n.restaurantId = :restaurantId and n.status = 'SENT'
-            order by n.createdAt des
+            order by n.createdAt desc
             """)
     List<Notification> getActiveNotificationsForRestaurant(UUID restaurantId);
 
