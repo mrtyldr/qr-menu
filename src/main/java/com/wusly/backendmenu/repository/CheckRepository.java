48f74fd69ecbffc2d8062579e12c7ab4d13d4c8c
Murat YILDIRIM <murat.yildirim@parga.li>
2023-04-18 14:03:48 +0300

    fix check not found

--- src/main/java/com/wusly/backendmenu/repository/CheckRepository.java
+++ src/main/java/com/wusly/backendmenu/repository/CheckRepository.java
@@ -3,6 +3,7 @@ package com.wusly.backendmenu.repository;
 import com.wusly.backendmenu.domain.check.Check;
 import com.wusly.backendmenu.domain.check.CheckStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -10,5 +11,10 @@ import java.util.UUID;
 public interface CheckRepository extends JpaRepository<Check, UUID> {
     boolean existsByTableIdAndStatus(UUID tableId, CheckStatus checkStatus);
 
+    @Query("""
+            select c 
+            from Check c 
+            where c.tableId = :tableId and c.status = :checkStatus
+            """)
     Optional<Check> findByTableIdAndStatus(UUID tableId, CheckStatus checkStatus);
 }
